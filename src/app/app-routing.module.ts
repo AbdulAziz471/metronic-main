cddee9abed458d85b79f7a1bf049e054b06d1525
aziztariq <aziztariq2211@gmail.com>
2024-10-25 04:27:23 -0700

    Extra File Remove 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,15 +12,8 @@ export const routes: Routes = [
     loadChildren: () =>
       import('./modules/errors/errors.module').then((m) => m.ErrorsModule),
   },
-  // {
-  //   path: '',
-  //   canActivate: [],
-  //   loadChildren: () =>
-  //     import('./_metronic/layout/layout.module').then((m) => m.LayoutModule),
-  // },
   {
     path: '',
-    // canActivate: [AuthGuard],
     loadChildren: () =>
       import('./_metronic/layout/layout.module').then((m) => m.LayoutModule),
   },
