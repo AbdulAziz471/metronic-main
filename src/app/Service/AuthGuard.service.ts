97814ea7fd8914be4e59ce730fd9474fe46e0e77
aziztariq <aziztariq2211@gmail.com>
2024-11-06 03:35:58 -0800

    Chnages

--- src/app/Service/AuthGuard.service.ts
+++ src/app/Service/AuthGuard.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthApiService } from './AuthApi.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthApiService, private router: Router) {}
 
-  canActivate(): boolean {
-    if (this.authService.isLoggedIn()) {
-      return true;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigate(['/auth']);
+      return false;
     }
-    this.router.navigate(['/auth']);
-    return false;
+
+    const requiredClaims = route.data['requiredClaims'];
+
+    if (requiredClaims && !this.authService.hasClaim(requiredClaims)) {
+    
+      this.router.navigate(['/error/403']);
+      return false;
+    }
+
+    return true;
   }
 }
